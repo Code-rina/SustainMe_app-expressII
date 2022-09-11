fd661c2ecfbb90a8e21172a9bcb4507718848758
Code-rina <katerina.kreibich@gmail.com>
2022-09-10 22:38:56 -0700

    HOme not rendering

--- authmeDemo/frontend/src/App.js
+++ authmeDemo/frontend/src/App.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import SignupFormPage from "./components/SignupFormPage";
 import Comments from "./components/Comments";
+import Home from "./components/Home"
 // import LoginFormPage from "./components/LoginFormPage";
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
@@ -10,23 +11,34 @@ import { Modal } from "./context/Modal";
 function App() {
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
-  const [showModal, setShowModal] = useState(false);
+  // const [showModal, setShowModal] = useState(false);
   useEffect(() => {
     dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
   }, [dispatch]);
 
+  if (!isLoaded) {
+    return null;
+  }
+
   return (
+    <BrowserRouter>
     <>
+      <Navigation />
+      <Switch>
+        <Route path='/' exact={true}>
+          <Home />
+        </Route>
       <Navigation isLoaded={isLoaded} />
-      <button onClick={() => setShowModal(true)}>Modal</button>
+      {/* <button onClick={() => setShowModal(true)}>Modal</button>
       <Comments />
       {showModal && (
         <Modal onClose={() => setShowModal(false)}>
           <h1>Hello I am a Modal</h1>
-        </Modal>
-      )}
+        </Modal> */}
+      {/* )} */}
       {isLoaded && (
         <Switch>
+
           {/* <Route path="/login" >
             <LoginFormPage />
           </Route> */}
@@ -35,7 +47,9 @@ function App() {
           </Route>
         </Switch>
       )}
+      </Switch>
     </>
+    </BrowserRouter>
   );
 }
 
